61beee1bace3a19b8e6639b3141e502869492464
nuczzz <nuc_zzz@163.com>
2018-12-13 23:54:37 +0800

    add some field of goroutine pool

--- goroutine_pool.go
+++ goroutine_pool.go
@@ -1,13 +1,23 @@
 package gopool
 
+import "sync"
+
 type GoroutinePool struct {
-	//coreGoroutineNum is the number of core goroutine.
+	//coreGoroutineNum the number of core goroutine.
 	//we create a new goroutine to handle the new request
 	//when goroutine number less than coreGoroutineNum,
 	//even if some goroutines are not working.
 	coreGoroutineNum int
 
-	//maxGoroutineNum is the max number of goroutine.
+	//maxGoroutineNum the max number of goroutine.
 	//maxGoroutineNum count all of the goroutines(include core goroutine).
 	maxGoroutineNum int
+
+	//IsWorking status of the goroutine,
+	//set IsWorking to true before handle request,
+	//we can call the goroutine to handle new request if IsWorking == false
+	IsWorking bool
+
+	//Lock lock of the goroutine.
+	Lock sync.Mutex
 }
